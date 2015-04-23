228f687d47040f721273534ecba06b2c47ab1957
allenj12 <allenj12@rpi.edu>
2015-04-23 16:59:30 -0400

    toy network up for identity function

--- features/featureNet.py
+++ features/featureNet.py
@@ -9,11 +9,16 @@ import Image
 import numpy as np
 import random
 import os
+import neurolab as nl
+
+
+# In[2]:
 
 os.chdir("..")
+get_ipython().magic(u'pwd')
 
 
-# In[13]:
+# In[3]:
 
 def getData(path):
     f = open(path)
@@ -21,20 +26,40 @@ def getData(path):
     f.close
     
     images = []
+    classification = []
     for line in lines:
         nums = line.split()
-        images.append([((float (val))) for val in nums])
-    print images[0]
+        classification.append(nums[0])
+        images.append([((float (val))) for val in nums[1:]])
+    return (classification,images)
 
 
-# In[14]:
+# In[4]:
 
-getData("ZipDigits.train.txt")
-
-
-# In[9]:
+classification,trainData = getData("ZipDigits.train.txt")
 
 
+# In[5]:
+
+# Create network with 256 inputs, 2 neurons in hidden layer
+# And 256 in output layer
+testData = trainData[:101]
+inputParams = [[-1, 1]] * len(trainData[0])
+ann = nl.net.newff(inputParams, [2, 256])
+
+# Train process
+err = ann.train(testData, testData,epochs=20,show=5)
+
+test = ann.sim(testData)
+
+print test
+
+
+
+# In[12]:
+
+print trainData[0][7]
+print test[0][7]
 
 
 # In[ ]:
